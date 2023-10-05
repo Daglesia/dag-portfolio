c8b5f7d3ae189669ab209562a2af8259bdd3441c
Daglesia <daglesia.gaming@gmail.com>
2023-10-05 15:07:34 +0200

    fix(routing): fix losing history before navigation to about

--- middleware/about.ts
+++ middleware/about.ts
@@ -3,6 +3,6 @@ import { PATHS } from "@/assets/constants/paths";
 export default defineNuxtRouteMiddleware((to, _from) => {
     const router = useRouter();
     if (to.path === PATHS.about.default) {
-        router.replace(PATHS.about.work);
+        router.push(PATHS.about.work);
     }
 });
